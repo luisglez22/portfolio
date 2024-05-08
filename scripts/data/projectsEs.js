753fea8c17cecb7fbd8700873f2c4a39bbd4b346
Luis González <luisglez2212@gmail.com>
2024-05-08 19:34:44 +0200

    Create spanish database

---
+++ scripts/data/projectsEs.js
@@ -0,0 +1,90 @@
+export const projectsEs = [{
+  id: 0,
+  number: 'zero',
+  href: 'khaos',
+
+  name: 'Kháos',
+  designType: 'Editorial,<br>Diseño Web',
+  year: '2023',
+  text: `
+  <p>Kháos tiene como objetivo mostrar que el universo es inherentemente impredecible y que quizás no todo tiene una explicación, ya que el caos es el estado natural de todo lo que nos rodea, donde lo predecible y lo impredecible coexisten.</p>
+  <p>También busca demostrar a través de esta premisa que la vida, tal y como la conocemos, es así por una gran cadena de casualidades.</p>
+  <p>Se trata de una contemplación altamente personal que adquiere significado y se desarrolla a través de un breve ensayo, complementado por un sitio web que pretende transmitir de manera visual la idea detrás del proyecto; también se realizó un breve Case Study explicativo. </p>
+  <p>Tanto la redacción del ensayo como su diseño y maquetación, junto con el sitio web complementario y el Manual de Identidad fueron desarrollados exclusivamente por mí, puesto que este proyecto fue presentado como mi Trabajo de Fin de Carrera.</p>
+  
+  <div class="links">
+    <a class="js-link" href="../assets/pdf/khaos/brand-manual_khaos.pdf" target="_blank">
+      <span class="js-point">·</span><p>Memoria y Manual de Identidad</p>
+    </a>
+    <a class="js-link" href="https://youtu.be/3qPBJP-CNIM?si=FNGYJ4AtiG72sLAV" target="_blank">
+      <span class="js-point">·</span><p>Case Study</p>
+    </a>
+  </div>
+  `,
+
+  videoHome: 'assets/video/khaos/khaos_book.mp4',
+  projectMedia: `
+  <video controls autoplay="autoplay" playsinline muted defaultMuted loop="loop" oncontextmenu="return false;" preload="auto" style="filter: brightness(0.97);"> 
+    <source src="../assets/video/khaos/khaos_case-study.mp4" type="video/mp4">
+  </video>
+  <video autoplay="autoplay" playsinline muted defaultMuted loop="loop" oncontextmenu="return false;" preload="auto" style="filter: brightness(0.9) contrast(1.1);"> 
+    <source src="../assets/video/khaos/khaos_book.mp4" type="video/mp4">
+  </video>
+  <div class="video-container">
+    <video data-can-pause="true" autoplay="autoplay" playsinline muted defaultMuted loop="loop" oncontextmenu="return false;" preload="auto" style="filter: brightness(0.97);"> 
+      <source src="../assets/video/khaos/khaos_web.mp4" type="video/mp4">
+    </video>
+  </div>
+  `,
+
+  titleIsLong: 'false'
+}, {
+  id: 1,
+  number: 'one',
+  href: 'memorial-rey-balduino',
+
+  name: 'Memorial<br>Rey Balduino',
+  designType: 'Diseño Web',
+  year: '2023',
+  text: `
+  <p>King Baudouin Memorial (Memorial Rey Balduino) located in Motril, aims to convey to the public the values and life philosophy of Baudouin, King of the Belgians, and to share with all who visit this place his human and spiritual journey.</p>
+  <p>My role involved crafting its website, adhering to the previously established graphic identity in order to achieve an environment that aligns with the essence of the memorial where elegance does not compromise modernity.</p>
+
+  <div class="links">
+    <a class="js-link" href="https://www.memorialreybalduino.com" target="_blank">
+      <span class="js-point">·</span><p>Ver sitio web</p>
+    </a>
+  </div>
+  `,
+
+  videoHome: 'assets/video/king-baudouin-memorial/king-baudouin-memorial_home.mp4',
+  projectMedia: `
+  <div class="video-container">
+  <video data-can-pause="true" autoplay="autoplay" playsinline muted defaultMuted loop="loop" oncontextmenu="return false;" preload="auto" style="filter: brightness(0.97); mix-blend-mode: screen;"> 
+    <source src="../assets/video/king-baudouin-memorial/king-baudouin-memorial_web.mp4">
+  </video>
+  </div>
+  `,
+
+  titleIsLong: 'true',
+  nameDesktopLineOne: `
+    <span>
+      <p>Memorial</p>
+    </span>
+  `,
+  nameDesktopLineTwo: `
+    <span>
+      <p>Rey Balduino</p>
+    </span>
+  `,
+  nameMobileLineOne: `
+    <span>
+      <p>Memorial</p>
+    </span>
+  `,
+  nameMobileLineTwo: `
+    <span>
+      <p>Rey</p>
+    </span>
+  `
+}]
